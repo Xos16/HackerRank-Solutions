daa53015153906a661e11716f40d35a49fcaca84
Attila Somogyvari <somogyvari.attila16@gmail.com>
2022-04-02 02:32:15 +0200

    hackerrank: birthday-cake-candles
    Signed-off-by: Attila Somogyvari <somogyvari.attila16@gmail.com>

---
+++ hackerrank/birthday-cake-candles/program.cs
@@ -0,0 +1,66 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {       
+        int result = 0;
+        int Maxnum = 0;        
+        int n = candles.Count;
+        
+        
+        Maxnum = candles[0];
+            for (int i = 0; i < n; i++)
+                if(candles[i] > Maxnum)
+                    Maxnum = candles[i];                    
+                    
+        for (int i = 0; i < n; i++)    
+            if (candles[i] == Maxnum)
+            {                
+                result++;
+            }                
+        //Console.WriteLine("Ertek: {0}", Maxnum);
+        //int result = 0; //arr2 index(2) helyettesito?        
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
